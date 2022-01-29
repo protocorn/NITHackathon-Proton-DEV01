15070d4de9af5157511dc3134a41850e06b9174f
protocorn <chordiasahil24@gmail.com>
2022-01-29 20:08:56 +0530

    drag and drop

--- app/src/main/java/com/example/websitemaker/MainActivity.java
+++ app/src/main/java/com/example/websitemaker/MainActivity.java
@@ -4,19 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.websitemaker.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     String code;
+    List<View> text_list = new ArrayList<View>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +36,33 @@ public class MainActivity extends AppCompatActivity {
 
         code=getIntent().getStringExtra("code");
 
+        binding.nxtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,FooterActivity.class);
+                intent.putExtra("code",code);
+                startActivity(intent);
+            }
+        });
+
         binding.vertical.setOnLongClickListener(longClickListener);
+        binding.horizontal.setOnLongClickListener(longClickListener);
+        binding.buttons.setOnLongClickListener(longClickListener);
+        binding.cards.setOnLongClickListener(longClickListener);
+        binding.txtArea.setOnLongClickListener(longClickListener);
 
         binding.webLayout.setOnDragListener(dragListener);
+
+        binding.webLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i=1;i<=text_list.size();i++) {
+                    if (view.getId() == i){
+                        Toast.makeText(MainActivity.this, ""+i, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
     View.OnLongClickListener longClickListener=new View.OnLongClickListener() {
@@ -49,18 +81,23 @@ public class MainActivity extends AppCompatActivity {
             int event = dragEvent.getAction();
 
             switch (event){
-                case DragEvent.ACTION_DRAG_ENTERED:
+                case DragEvent.ACTION_DROP:
                     final View view1=(View) dragEvent.getLocalState();
-                    if(view1.getId()==R.id.vertical){
-                        Toast.makeText(MainActivity.this, "Drag Success", Toast.LENGTH_SHORT).show();
+                    if(view1.getId()==R.id.txt_area){
+                        EditText textView=new EditText(MainActivity.this);
+                        textView.setLayoutParams(new ViewGroup.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textView.setX(dragEvent.getX());
+                        textView.setY(dragEvent.getY());
+                        binding.webLayout.addView(textView);
+                        textView.setText("text - area"+(text_list.size()+1));
+                        text_list.add(textView);
+                        textView.setId(text_list.size());
                     }
-                    break;
-                    case DragEvent.ACTION_DRAG_EXITED:
-                    break;
-                    case DragEvent.ACTION_DROP:
-                        break;
+                    else if(view1.getId()==R.id.buttons){
+
+                    }
             }
-            return false;
+            return true;
         }
     };
 }
