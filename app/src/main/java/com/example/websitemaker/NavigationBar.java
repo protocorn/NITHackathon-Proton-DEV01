2508ef881d7a496b2e0376f8e2b95487d537ab61
protocorn <chordiasahil24@gmail.com>
2022-01-29 16:48:51 +0530

    layout

--- app/src/main/java/com/example/websitemaker/NavigationBar.java
+++ app/src/main/java/com/example/websitemaker/NavigationBar.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -32,6 +33,7 @@ public class NavigationBar extends AppCompatActivity {
 
         EditText n_title,img_url,item1,item2,item3,dropdown_count;
         LinearLayout n_items,dd_items;
+        Button done_btn;
 
         n_title=dialog.findViewById(R.id.nav_title);
         img_url=dialog.findViewById(R.id.image_url);
@@ -43,6 +45,8 @@ public class NavigationBar extends AppCompatActivity {
         n_items=dialog.findViewById(R.id.nav_items);
         dd_items=dialog.findViewById(R.id.dropdown_items);
 
+        done_btn=dialog.findViewById(R.id.done);
+
 
         code.append("<!DOCTYPE html>\n<html>\n<head>\n<title>"+title+"</title>\n</head>\n<body>");
 
@@ -50,8 +54,44 @@ public class NavigationBar extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dialog.show();
+               String navbar_title=n_title.getText().toString();
+               done_btn.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       if(!navbar_title.isEmpty()){
+                           code.append("<nav class=\"navbar navbar-light bg-light\">\n  <div class=\"container-fluid\">\n" +
+                                   " <span class=\"navbar-brand mb-0 h1\">"+navbar_title+"</span>\n  </div> \n </nav>");
+                       }
+                   }
+               });
+
             }
         });
 
+        binding.nav2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+                img_url.setVisibility(View.VISIBLE);
+                String navbar_title=n_title.getText().toString();
+                String url=img_url.getText().toString();
+
+                done_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!navbar_title.isEmpty() && !url.isEmpty()){
+                            code.append("<nav class=\"navbar navbar-light bg-light\">\n" +
+                                    "  <div class=\"container\">\n" +
+                                    "    <a class=\"navbar-brand\" href=\"#\">\n" +
+                                    "      <img src=\""+url+"\" alt=\"\" width=\"30\" height=\"24\">\n" +
+                                    "    </a>\n" +
+                                    "  </div>\n" +
+                                    "</nav>");
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
